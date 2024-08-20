0728a5df785e8a84ea0ddfb6f2a29775c1b2a0ad
Gert-Jan van Dijk <gert-jan.van.dijk@thunderbyte.ai>
2024-08-20 11:44:35 +0200

    update to wefitter lib v0.1.1

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -40,18 +40,14 @@ export default function App() {
 
   // create config
   const config = {
-    token:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
-      'eyJpc3MiOiJHSi1URVNUIiwiYXBwIjoiYTI3NTlkMzktYTM4Ni00NWQ3LThkYTItN' +
-      'WQzOTExMjdhMDNjIiwiaWF0IjoxNzE4NDMxOTkxLCJpZCI6IjAwYmJkZmM1LTY5YzUtNDZkNy1iZWQ3LTdjOTliNTI2MmU4NCJ9.' +
-      '52lnJw6BmIqyQtrV1AJ_KEcZhnYOkh0nx4WsoDyujps', // required, WeFitter API profile bearer token
-    //apiUrl: 'YOUR_API_URL', // optional, only use if you want to use your backend as a proxy and forward all API calls to the WeFitter API. Default: `https://api.wefitter.com/api/`
-    //startDate: 'CUSTOM_START_DATE', // optional with format `yyyy-MM-dd`, by default data of the past 20 days will be uploaded
-    //notificationTitle: 'CUSTOM_TITLE', // optional
-    //notificationText: 'CUSTOM_TEXT', // optional
-    //notificationIcon: 'CUSTOM_ICON', // optional, e.g. `ic_notification` placed in either drawable, mipmap or raw
-    //notificationChannelId: 'CUSTOM_CHANNEL_ID', // optional
-    //notificationChannelName: 'CUSTOM_CHANNEL_NAME', // optional
+    token: 'YOUR_BEARER_TOKEN', // required, WeFitter API profile bearer token
+    apiUrl: 'YOUR_API_URL', // optional, only use if you want to use your backend as a proxy and forward all API calls to the WeFitter API. Default: `https://api.wefitter.com/api/`
+    startDate: 'CUSTOM_START_DATE', // optional with format `yyyy-MM-dd`, by default data of the past 20 days will be uploaded
+    notificationTitle: 'CUSTOM_TITLE', // optional
+    notificationText: 'CUSTOM_TEXT', // optional
+    notificationIcon: 'CUSTOM_ICON', // optional, e.g. `ic_notification` placed in either drawable, mipmap or raw
+    notificationChannelId: 'CUSTOM_CHANNEL_ID', // optional
+    notificationChannelName: 'CUSTOM_CHANNEL_NAME', // optional
     appPermissions: myAppPermissionsString,
   };
 
