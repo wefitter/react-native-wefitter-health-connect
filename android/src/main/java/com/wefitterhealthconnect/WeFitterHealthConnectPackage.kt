1d9d94073f4e47ba35b3f663d7e48a48bc0a13de
Gert-Jan van Dijk <gert-jan.van.dijk@thunderbyte.ai>
2026-02-05 10:40:23 +0100

    First RN 0.81 version

--- android/src/main/java/com/wefitterhealthconnect/WeFitterHealthConnectPackage.kt
+++ android/src/main/java/com/wefitterhealthconnect/WeFitterHealthConnectPackage.kt
@@ -1,16 +1,22 @@
 package com.wefitterhealthconnect
 
+import android.view.View
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ReactShadowNode
 import com.facebook.react.uimanager.ViewManager
 
 class WeFitterHealthConnectPackage : ReactPackage {
-  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-    return listOf(WeFitterHealthConnectModule(reactContext))
-  }
 
-  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
-    return emptyList()
-  }
+  @Deprecated("Migrate to [BaseReactPackage] and implement [getModule] instead.")
+  override fun createNativeModules(
+    reactContext: ReactApplicationContext
+  ): MutableList<NativeModule> = listOf(WeFitterHealthConnectModule(reactContext)).toMutableList()
+
+
+  override fun createViewManagers(
+    reactContext: ReactApplicationContext
+  ): MutableList<ViewManager<View, ReactShadowNode<*>>> = mutableListOf()
+
 }
--- example/android/app/src/main/java/com/wefitterhealthconnectexample/MainApplication.kt
+++ example/android/app/src/main/java/com/wefitterhealthconnectexample/MainApplication.kt
@@ -13,51 +13,75 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
 
 class MainApplication : Application(), ReactApplication {
-    private val mReactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
-        override fun getUseDeveloperSupport(): Boolean {
-            return BuildConfig.DEBUG
+  @Deprecated(
+    "You should not use ReactNativeHost directly in the New Architecture. Use ReactHost instead.",
+    replaceWith = ReplaceWith("reactHost")
+  )
+  override val reactNativeHost: ReactNativeHost =
+    object : DefaultReactNativeHost(this) {
+      override fun getPackages(): List<ReactPackage> =
+        PackageList(this).packages.apply {
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // add(MyReactNativePackage())
         }
 
-        override fun getPackages(): List<ReactPackage> {
-            val packages: List<ReactPackage> = PackageList(this).packages
-            // Packages that cannot be autolinked yet can be added manually here, for example:
-            // packages.add(new MyReactNativePackage());
-            return packages
-        }
+      override fun getJSMainModuleName(): String = "index"
 
-        override fun getJSMainModuleName(): String {
-            return "index"
-        }
+      override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-        override val isNewArchEnabled: Boolean
-            get() = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+      override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
 
-        override val isHermesEnabled: Boolean
-            get() = BuildConfig.IS_HERMES_ENABLED
+      override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+    }
+  /*
+  private val mReactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
+    override fun getUseDeveloperSupport(): Boolean {
+      return BuildConfig.DEBUG
     }
 
-    override fun getReactNativeHost(): ReactNativeHost {
-        return mReactNativeHost
+    override fun getPackages(): List<ReactPackage> {
+      val packages: List<ReactPackage> = PackageList(this).packages
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+      return packages
     }
 
-    override fun onCreate() {
-        super.onCreate()
-        SoLoader.init(this,  /* native exopackage */false)
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            // If you opted-in for the New Architecture, we load the native entry point for this app.
-            load()
-        }
-        ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
+    override fun getJSMainModuleName(): String {
+      return "index"
     }
 
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-    override fun registerReceiver(receiver: BroadcastReceiver?, filter: IntentFilter): Intent? {
-        return if (Build.VERSION.SDK_INT >= 34 && applicationInfo.targetSdkVersion >= 34) {
-            super.registerReceiver(receiver, filter, RECEIVER_EXPORTED)
-        } else {
-            super.registerReceiver(receiver, filter, RECEIVER_NOT_EXPORTED)
-        }
+    override val isNewArchEnabled: Boolean
+      get() = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+
+    override val isHermesEnabled: Boolean
+      get() = BuildConfig.IS_HERMES_ENABLED
+  }
+
+  override fun getReactNativeHost(): ReactNativeHost {
+    return mReactNativeHost
+  }
+
+   */
+
+  override fun onCreate() {
+    super.onCreate()
+    SoLoader.init(this,  /* native exopackage */OpenSourceMergedSoMapping)
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      // If you opted-in for the New Architecture, we load the native entry point for this app.
+      load()
     }
+    // ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
+  }
+
+  @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+  override fun registerReceiver(receiver: BroadcastReceiver?, filter: IntentFilter): Intent? {
+    return if (Build.VERSION.SDK_INT >= 34 && applicationInfo.targetSdkVersion >= 34) {
+      super.registerReceiver(receiver, filter, RECEIVER_EXPORTED)
+    } else {
+      super.registerReceiver(receiver, filter, RECEIVER_NOT_EXPORTED)
+    }
+  }
 }
